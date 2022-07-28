b932dad54cc6cb517ef2f3cfc8b931c3175a7c37
Anton Dudnik <tonydudniq@gmail.com>
2022-07-28 08:35:56 +0300

    Update to Lesson 35

---
+++ code1.php
@@ -0,0 +1,113 @@
+<?php
+//Lesson 35. Task 1
+$test = 0;
+
+if (empty($test)) {
+    echo '+';
+} else {
+    echo '-';
+}
+//Lesson 35. Task 2
+$test = -1;
+
+if (empty($test)) {
+    echo '+';
+} else {
+    echo '-';
+}
+//Lesson 35. Task 3
+$test = '';
+
+if (!empty($test)) {
+    echo '+';
+} else {
+    echo '-';
+}
+//Lesson 35. Task 4
+$test = -1;
+
+if (empty($test)) {
+    echo '+';
+} else {
+    echo '-';
+}
+//Lesson 35. Task 5
+$test = '0';
+
+if (!empty($test)) {
+    echo '+';
+} else {
+    echo '-';
+}
+//Lesson 35. Task 6
+$test = -1;
+
+if (!empty($test)) {
+    echo '+';
+} else {
+    echo '-';
+}
+//Lesson 35. Task 7
+$test = null;
+
+if (empty($test)) {
+    echo '+';
+} else {
+    echo '-';
+}
+//Lesson 35. Task 8
+$test = false;
+
+if (!empty($test)) {
+    echo '+';
+} else {
+    echo '-';
+}
+//Lesson 35. Task 9
+$test = true;
+
+if (!empty($test)) {
+    echo '+';
+} else {
+    echo '-';
+}
+//Lesson 35. Task 10
+$test = 'false';
+
+if (!empty($test)) {
+    echo '+';
+} else {
+    echo '-';
+}
+//Lesson 35. Task 11
+$test = 'null';
+
+if (!empty($test)) {
+    echo '+';
+} else {
+    echo '-';
+}
+//Lesson 35. Task 12
+$test = 0;
+
+if (isset($test)) {
+    echo '+';
+} else {
+    echo '-';
+}
+//Lesson 35. Task 13
+$test = null;
+
+if (isset($test)) {
+    echo '+';
+} else {
+    echo '-';
+}
+//Lesson 35. Task 14
+$test = false;
+
+if (isset($test)) {
+    echo '+';
+} else {
+    echo '-';
+}
