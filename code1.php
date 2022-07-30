f5e00ec1d207843a49cddd51de942fe0fdae25a8
Anton Dudnik <tonydudniq@gmail.com>
2022-07-30 21:08:49 +0300

    Update to Lesson 60

--- code1.php
+++ code1.php
@@ -557,6 +557,51 @@ for($i = 0; $i <10; $i++) {
 }echo $str;
 echo '<br>';
 //Lesson 59. Task 1
-
+for( $i = 1; $i <= 9; $i++) {
+    for($j = 1; $j <= 3; $j++) {
+        echo $i;
+    }
+}echo '<br>';
 //Lesson 59. Task 2
+for( $i = 10; $i <= 30; $i += 10) {
+    for($j = 1; $j <= 3; $j++) {
+        echo $i +$j . ' ';
+    }
+}
+//Lesson 60. Task 1
+$arr = [];
 
+$arr[] = 1;
+$arr[] = 2;
+$arr[] = 3;
+$arr[] = 4;
+$arr[] = 5;
+$arr[] = 6;
+$arr[] = 7;
+$arr[] = 8;
+$arr[] = 9;
+$arr[] = 10;
+var_dump($arr);
+//Lesson 60. Task 2
+$arr = [];
+$arr[] = 'x';
+$arr[] = 'x';
+$arr[] = 'x';
+$arr[] = 'x';
+$arr[] = 'x';
+var_dump($arr);
+echo '<br>';
+//Lesson 60. Task 3
+$arr = [];
+for($i = 0; $i < 100; $i++) {
+    $arr[] = $i + 1;
+}var_dump($arr);
+echo '<br>';
+echo '<br>';
+//Lesson 60. Task 4
+$arr = [];
+for($i = 0; $i<100;$i++) {
+    if($i % 2 == 0) {
+        $arr[] = $i +1;
+    }
+}var_dump($arr);
