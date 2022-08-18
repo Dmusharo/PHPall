1b8928cff5ca6b47d74615dfca17eef56aeae7e3
Dmusharo <dmusharo@gmail.com>
2022-08-18 19:16:23 +0300

    calc add file

--- calc.php
+++ calc.php
@@ -1,4 +1,7 @@
 <meta charset="utf-8">
+<html lang="en" >
+<head>
+    <title>Калькулятор</title>
 </head>
 <body>
 <h2>Калькулятор</h2>
@@ -18,31 +21,27 @@
 </html>
 <?php
 
+
+
+
+$number=5;
+function squearFuncReturn(){
+    global $number;
+    $number**=2;
+    echo "<h1>Результат, $number</h1>";
+
+}
+squearFuncReturn();
+
+$a=5;
+$b=6;
+function sum(){
+    global $a;
+    global $b;
+    $s=0;
+    $s=$a+$b;
+    echo $s;
+}
+sum();
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
