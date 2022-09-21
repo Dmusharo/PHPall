d9f7df9830acc7faddc66cf5d7c4577fe74f466e
Dmusharo <dmusharo@gmail.com>
2022-09-21 19:51:24 +0300

    73 Конвертация многомерных массивов в PHP

--- index.php
+++ index.php
@@ -1,60 +1,47 @@
-<?php
+    <?php
+    if (!empty($_POST['name']) and !empty($_POST['surname']) and !empty($_POST['mail']) and !empty($_POST['password'])) {
+        // Get data from form to $
+        $name=$_POST['name'];
+        $surname=$_POST['surname'];
+        $mail=$_POST['mail'];
+        $password=$_POST['password'];
+        // connection to database
+        $user = "root";
+        $pass = "password";
+        $host = "db";
+        $dbdb = "db";
+        $connection = new mysqli($host, $user, $pass, $dbdb);
+        // Query
+        $query = "SELECT * FROM users WHERE email='$mail'";
+        $mailinbase=mysqli_fetch_assoc(mysqli_query($connection,$query));
+        // Check
+        if (empty($mailinbase)) {
+            $connection->query("INSERT INTO `users` (`first_name`, `last_name`, `email`, `password`) VALUES ('$name', '$surname', '$mail', '$password');");
+            echo "Hello, ".$name;
 
-/*
- * 1 Возведите 2 в 10 степень.
- */
-
-echo pow(2,10).'<br>';
-/*
- * 2 Найдите квадратный корень из 245.
- */
-
-echo  sqrt(245).'<br>';;
-
-
-/*
- * 3 <?php
-	$arr = [4, 2, 5, 19, 13, 0, 10];
-?>
-Найдите корень из суммы квадратов его элементов. Для решения воспользуйтесь циклом foreach.
- */
-$arr = [1,2];
-$result=0;
-
-
-foreach ($arr as $item) {
-    $sqrtValue=pow($item,2);
-    $result+=$sqrtValue;
+             } else {
+            echo "Пользователь с таким E-mail  уже существует в базе";
+        }
     }
- echo sqrt($result);
+    ?>
 
+    <html>
+    <head>
+        <title>Enter page</title>
+    </head>
+    <body>
+    <form action="" method="post">
+        Name:<input type="text" name="name" required>
+        <br>
+        Surname:<input type="text" name="surname" required>
+        <br>
+        E-mail:<input type="text" name="mail" required>
+        <br>
+        Password:<input type="password" name="password" required>
+        <br>
+        <input type="submit" value="Create" name="submit">
 
-
-/*
- * 4 Найдите квадратный корень из 379. Результат округлите до целых, до десятых, до сотых.
- */
-
-
-/*
- * 5 Найдите квадратный корень из 587. Округлите результат в большую и меньшую сторону, запишите результаты округления в ассоциативный массив с ключами 'floor' и 'ceil'.
- */
-
-/*
- * 6  <?php
-	$arr = [4, -2, 5, 19, -130, 0, 10];
-?>
-Найдите минимальное и максимальное число.
- */
-
-/*
- * 7 Выведите на экран случайное целое число от 1 до 10
- */
-
-/*
- * 8 С помощью цикла заполните массив 10-ю случайными целыми числами
- */
-
-/*
- * 9   Даны переменные $a и $b. Найдите найдите модуль разности $a и $b. Проверьте работу скрипта самостоятельно для различных $a и $b.
- */
+    </form>
+    </body>
+    </html>
 
