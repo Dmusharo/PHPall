92862e17a7216a00272a45a5d9d1b4c3f239e9a2
Dmusharo <dmusharo@gmail.com>
2022-10-06 09:31:12 +0300

    95 Фигурные скобки в регулярных выражения PHP { }

--- Homework/92 Операторы повторения символов в регулярках.php
+++ Homework/92 Операторы повторения символов в регулярках.php
@@ -55,3 +55,4 @@ echo preg_replace('#ab?a#','!',$str); //Ноль или раз
 //Напишите регулярку, которая найдет строки 'aa', 'aba', 'abba', 'abbba', не захватив 'abca' и 'abea'
 $str = 'aa aba abba abbba abca abea';
 echo preg_replace('#ab*a#','!',$str);
+
