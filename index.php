9f7c4b8d54a57cfa84430c02817a6c69786ec1b8
Dmusharo <dmusharo@gmail.com>
2022-10-06 18:51:43 +0300

    112 Автоматическое тестирование регулярок в PHP

--- index.php
+++ index.php
@@ -44,4 +44,14 @@
     </form>
     </body>
     </html>
+<?php
+    $reg   = '#\d,\d#'; // ваша регулярка
 
+    $arr[] = 'aaa 123 bbb 0,1';   //
+    $arr[] = 'aaa 12345 bbb 3,3'; //
+    $arr[] = 'aaa 12x bbb';   //
+    $arr[] = 'aaa 12 bbb';    //
+
+    foreach ($arr as $str) {
+    echo $str . ' ' . preg_match($reg, $str) . '<br>';
+    }
