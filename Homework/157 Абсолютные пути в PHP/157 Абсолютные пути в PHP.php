f7056bde86e3fa16dbfafe43b870f6bc4ce6106d
Dmusharo <dmusharo@gmail.com>
2022-10-09 14:28:13 +0300

    157 Абсолютные пути в PHP file_get_contents('../directory/test.txt');

---
+++ Homework/157 Абсолютные пути в PHP/157 Абсолютные пути в PHP.php
@@ -0,0 +1,9 @@
+<?php
+//echo $_SERVER['DOCUMENT_ROOT'];
+//echo $_SERVER['DOCUMENT_ROOT'];
+
+//$root = $_SERVER['DOCUMENT_ROOT'];
+//echo file_get_contents($root . '/directory/test.txt');
+
+echo __DIR__;
+echo __FILE__;
