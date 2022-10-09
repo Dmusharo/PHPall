d203afcaefec9b78c7aeac5d57600d723af20435
Dmusharo <dmusharo@gmail.com>
2022-10-09 12:15:16 +0300

    155 Чтение и запись файла в PHP  file_get_contents  file_put_contents

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
 
 <?php
-$str =$arr = [ '30-11-1995', '29-10-1990','31-12-2025' ];
-$str=null;
-foreach ($arr as $elem=>$key)
-{
-    $str.=$key.' ';
-}
-echo '<br>'. $str . '<br>';
-preg_match_all('#(?<days>\d{2})-(?<month>\d{2})-(?<years>19(9\d)|20([0-9]\d))#',$str,$res);
-print_r ($res[1]);
-echo '<br>';
-print_r ($res[2]);
-echo '<br>';
-print_r ($res[3]);
+
+
