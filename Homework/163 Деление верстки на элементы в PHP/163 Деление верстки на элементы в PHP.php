c80fabbb0f9da17b45c348e5235644c9a0d0d6ee
Dmusharo <dmusharo@gmail.com>
2022-10-10 19:40:33 +0300

    163 Деление верстки на элементы в PHP

---
+++ Homework/163 Деление верстки на элементы в PHP/163 Деление верстки на элементы в PHP.php
@@ -0,0 +1,24 @@
+<?php
+//Вставка файлов в PHP
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>title</title>
+</head>
+<body>
+<header>
+    <?php include 'Homework/163 Деление верстки на элементы в PHP/header.php'?>
+</header>
+<aside>
+    <?php include 'Homework/163 Деление верстки на элементы в PHP/sidebar.php'?>
+</aside>
+<main>
+    content
+</main>
+<header>
+    footer
+</header>
+</body>
+</html>
