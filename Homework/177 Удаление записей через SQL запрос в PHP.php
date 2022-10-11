949bad23537660d33768bb1eca92ba352c849363
Dmusharo <dmusharo@gmail.com>
2022-10-11 14:04:04 +0300

    177 Удаление записей через SQL запрос в PHP

--- Homework/177 Удаление записей через SQL запрос в PHP.php
+++ Homework/177 Удаление записей через SQL запрос в PHP.php
@@ -1,1 +1,32 @@
+<meta charset="utf-8">
 <?php
+// connection to database
+$user = "root"; // имя пользователя
+$pass = "password"; // пароль
+$host = "db"; //имя хоста
+$name = "mydb"; //имя базы данныз
+$connection = new mysqli($host, $user, $pass, $name);
+mysqli_query($connection, "SET NAMES 'utf8'");
+//1  Удалите юзера с id, равным 7.
+//$query = 'DELETE FROM users WHERE id=7';
+
+//2  Удалите всех юзеров, у которых возраст 23 года.
+//$query = 'DELETE FROM users WHERE age=23';
+
+//3 Удалите всех юзеров
+$query='DELETE FROM users' ;
+
+
+$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+
+//Формируем массив в цикле
+echo '----------------------'.'<br>';
+for ($data=[];$row=mysqli_fetch_assoc($result); $data[]=$row);
+echo '<pre>';
+var_dump($data);
+echo '<pre>';
+
+
+
+?>
+
