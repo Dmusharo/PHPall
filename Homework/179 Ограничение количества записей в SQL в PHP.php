50429cdb447f6f273fb71ebef219ca2994f04516
Dmusharo <dmusharo@gmail.com>
2022-10-11 14:38:28 +0300

    179  Ограничение количества записей в SQL в PHP

--- Homework/179 Ограничение количества записей в SQL в PHP.php
+++ Homework/179 Ограничение количества записей в SQL в PHP.php
@@ -7,8 +7,17 @@ $host = "db"; //имя хоста
 $name = "mydb"; //имя базы данныз
 $connection = new mysqli($host, $user, $pass, $name);
 mysqli_query($connection, "SET NAMES 'utf8'");
-$query='SELECT * FROM users LIMIT 2';
+//1
+//Получите первых 4 юзера.
 
+//$query='SELECT * FROM users LIMIT 4';
+
+//2 Получите юзеров со второго, 3 штуки.
+//$query='SELECT * FROM users LIMIT 2,3';
+
+//3
+//Отсортируйте юзеров по возрастанию зарплаты и получите первых 3 работника из результата сортировки.
+$query='SELECT * FROM users order by salary  LIMIT 2,3';
 $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
 
 //Формируем массив в цикле
