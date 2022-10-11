3734e4831805d06f5791a723153cb5bce79209af
Dmusharo <dmusharo@gmail.com>
2022-10-11 11:59:07 +0300

    168-169  ВВедениев базы данных и Подготовительные манипуляции для работы с SQL в PHP

--- Homework/168 Введение в  базы данных.php
+++ Homework/168 Введение в  базы данных.php
@@ -1,1 +1,30 @@
 <?php
+// connection to database
+$user = "root";
+$pass = "password";
+$host = "db";
+$dbdb = "db";
+$connection = new mysqli($host, $user, $pass, $dbdb);
+
+
+// connection to database
+$user = "root"; // имя пользователя
+$pass = "password"; // пароль
+$host = "db"; //имя хоста
+$name = "mydb"; //имя базы данныз
+$connection = new mysqli($host, $user, $pass, $name);
+//-----------
+//    $result=mysqli_query($connection, 'SELECT * FROM users');
+//    echo '<pre>';
+//    var_dump($result);
+//    echo '<pre>';
+
+//Подготовительные манипуляции для работы с SQL в PHP
+// Отлавливаем ошибки базы данных
+$query = 'SELECT * FROM users WHERE salary <500';
+$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+echo '<pre>';
+var_dump($result);
+echo '<pre>';
+
+
