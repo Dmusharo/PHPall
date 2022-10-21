5671eaba39d8cfa5a3f952ba6308d0615394cd8a
Dmusharo <dmusharo@gmail.com>
2022-10-21 14:30:46 +0300

    198 Введение в редиректы PHP

--- Homework/Основной учебник PHP8+/198 Введение в редиректы PHP.php
+++ Homework/Основной учебник PHP8+/198 Введение в редиректы PHP.php
@@ -1,9 +1,11 @@
 <?php
+//Введение в редиректы PHP
+
 /* Перенаправление браузера на другую страницу в той же директории, что и
 изначально запрошенная */
-$host  = $_SERVER['HTTP_HOST'];
-$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$host = $_SERVER['HTTP_HOST'];
+$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 $extra = 'save.php';
 header("Location: http://$host$uri/$extra");
 exit;
-?>
+
